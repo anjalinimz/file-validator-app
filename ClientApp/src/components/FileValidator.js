a1fe2e0a99909271c62e94707799a315786d434f
Anjali Wijesinghe <anjalinimz@gmail.com>
2022-10-21 23:47:37 +0300

    Implemented file upload logic and frontend changes

--- ClientApp/src/components/FileValidator.js
+++ ClientApp/src/components/FileValidator.js
@@ -1,20 +1,56 @@
-import React, { Component } from 'react';
+import {useState} from 'react';
+import axios from 'axios';
+import { toast} from 'react-toastify';
 
-export class FileValidator extends Component {
-  static displayName = FileValidator.name;
+import './style.css';
 
-  render() {
+export const FileValidator = ({onSuccess}) => {
+    const [files, setFiles] = useState([]);
+
+    const onInputChange = (e) => {
+        setFiles(e.target.files)
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const data = new FormData();
+
+        for(let i = 0; i < files.length; i++) {
+            data.append('file', files[i]);
+        }
+        
+        axios.post('//localhost:8000/upload', data)
+            .then((response) => {
+                toast.success('Upload Success');
+                onSuccess(response.data)
+            })
+            .catch((e) => {
+                toast.error('Upload Error')
+            })
+    };
+
     return (
-      <div>
-        <h1>Smart File Validator</h1>
-        <p>Welcome to your File Validator</p>
-        <p>To help you get started, we have also set up:</p>
+        <div>
+        <h1>File Validator</h1>
+        <p>Welcome to your smart file validator</p>
+        <p>To get started, please follow the instruction given below,</p>
         <ul>
           <li><strong>Upload</strong>. Upload your zip file to the app</li>
           <li><strong>Validate</strong>. Validate the file</li>
           <li><strong>Save</strong>. Save the file</li>
         </ul>
-       </div>
-    );
-  }
-}
+        <form method="post" action="#" id="#" onSubmit={onSubmit}>
+            <div className="form-group files">
+                <label>Upload Your File </label>
+                <input type="file"
+                       onChange={onInputChange}
+                       className="form-control"
+                       multiple/>
+            </div>
+
+            <button>Save</button>
+        </form>
+        </div>
+    )
+};
--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,29 +1,45 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace file_validator_app.Controllers;
 
-[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class FileController : ControllerBase
 {
     private readonly ILogger<FileController> _logger;
 
-    [HttpPost("ImportFile")]
-    public async Task <IActionResult> ImportFile([FromForm] IFormFile file) {
-        
-        string name = file.FileName;
-        string extension = Path.GetExtension(file.FileName);
+    public FileController(ILogger<FileController> logger)
+    {
+        _logger = logger;
+    }
 
-        _logger.LogInformation(name + extension + " file import started...");
+/*
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55)
+        })
+        .ToArray();
+    }
+*/
+    [HttpPost]
+    public async Task<IActionResult> ImportFile([FromForm] IFormFile file) {
+        
+          _logger.LogInformation("file import started...");
+
+        string name = file.FileName;
+
+        _logger.LogInformation(name + " file import started...");
 
         //read the file
         await using(var memoryStream = new MemoryStream()) {
             file.CopyTo(memoryStream);
         }
 
-        _logger.LogInformation(name + extension + " file import completed...");
+        _logger.LogInformation(name + " file import completed...");
 
         return NoContent();
     }
