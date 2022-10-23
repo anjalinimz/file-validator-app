98b6e07c4647a23822ab068ab4b3e6c719169a61
Anjali Wijesinghe <anjalinimz@gmail.com>
2022-10-23 12:21:13 +0300

    removed unwanted files and configurations

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,12 +1,10 @@
-import ApiAuthorzationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { FileValidator } from "./components/FileValidator";
 
 const AppRoutes = [
   {
     index: true,
     element: <FileValidator />
-  },
-  ...ApiAuthorzationRoutes
+  }
 ];
 
 export default AppRoutes;
--- Models/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace file_validator_app.Models;
-
-public class ApplicationUser : IdentityUser
-{
-}
