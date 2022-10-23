573ef3424abea4c0d9a204c92f64c5311e77d895
Anjali Wijesinghe <anjalinimz@gmail.com>
2022-10-24 01:59:44 +0300

    modified ZipFileValidatorService

--- Data/Services/ZipFileValidatorService.cs
+++ Data/Services/ZipFileValidatorService.cs
@@ -44,26 +44,36 @@ public class ZipFileValidatorService : IValidatorService
             _logger.LogInformation(node);
 
             // contruct the zip strucuture by populating the directoty contents
-            if(x.Length>2 && !string.IsNullOrEmpty(x[2]))
+            if(x.Length>2)
             {
-                if(string.Equals(x[1],"dlls"))
-                {
-                    dllFound = true;
-                    zipStructure.dllsContent.Add(x[2]);
-                } else if(string.Equals(x[1],"images"))
-                {
-                    imageFound = true;
-                    zipStructure.imagesContent.Add(x[2]);
-                } else if(string.Equals(x[1],"languages"))
-                {
-                    langFound = true;
-                    zipStructure.languagesContent.Add(x[2]);
-                } else 
-                {
-                    // if there were directories, other than dlls, images and languages, will add a structure error. 
-                    zipStructure.structureErrors.Add(node+ " folder/file does not adhere to the correct strucure.");
-                }
-            }
+                    if(string.Equals(x[1],"dlls"))
+                    {
+                        dllFound = true;
+                        
+                        if(!string.IsNullOrEmpty(x[2]))
+                            zipStructure.dllsContent.Add(x[2]);
+
+                    } else if(string.Equals(x[1],"images"))
+                    {
+                        imageFound = true;
+
+                        if(!string.IsNullOrEmpty(x[2]))
+                            zipStructure.imagesContent.Add(x[2]);
+
+                    } else if(string.Equals(x[1],"languages"))
+                    {
+                        langFound = true;
+
+                        if(!string.IsNullOrEmpty(x[2]))
+                            zipStructure.languagesContent.Add(x[2]);
+
+                    } else 
+                    {
+                        // if there were directories, other than dlls, images and languages, will add a structure error. 
+                        zipStructure.structureErrors.Add(node+ " folder/file does not adhere to the correct strucure.");
+                    }
+            } 
+            
         }
 
         // if one of the must have directories are missing, will throw an exception immediately. 
@@ -95,10 +105,10 @@ public class ZipFileValidatorService : IValidatorService
         var errors = new List<String>();
         
         if(!dllsContent.Any())
-            errors.Add("Dlls should not be empty.");
+            errors.Add("dlls directory should not be empty");
     
         if(!dllsContent.Contains(rootFolderName+".dll"))
-            errors.Add("Dlls directory should have a dll file with the name '" +rootFolderName+ "'");
+            errors.Add("dlls directory must contain '" +rootFolderName+ ".dll'");
 
         return errors;
     }
@@ -108,13 +118,16 @@ public class ZipFileValidatorService : IValidatorService
         var errors = new List<String>();
         
         if(!imagesContent.Any())
-            errors.Add("Images should not be empty.");
+            errors.Add("No images in ‘images’ directory. Please add images.");
 
         foreach(string file in imagesContent)
         {
             if(!(file.Contains(".png") || file.Contains(".jpg")))
+            {
                 errors.Add("File types except .png or .jpg are not allowed in Images directory");
                 break;
+            }
+                
         }
 
         return errors;
@@ -125,17 +138,17 @@ public class ZipFileValidatorService : IValidatorService
         var errors = new List<String>();
 
         if(!languagesContent.Any())
-            errors.Add("Languages directory should not be empty.");
+            errors.Add("languages directory should not be empty");
 
         if(!languagesContent.Contains(rootFolderName +"_en.xml"))
-            errors.Add("Languages directory should have " + rootFolderName + "_en.xml");
+            errors.Add("languages directory must contain " + rootFolderName + "_en.xml");
 
         var filePatternRegex = new Regex(rootFolderName + "_[a-z]{2}.xml");
         foreach(string file in languagesContent)
         {
             if(!filePatternRegex.IsMatch(file))
             {
-                errors.Add("Languages directory files should follow  '" + rootFolderName + "_xx.xml format");
+                errors.Add("languages directory files should follow  '" + rootFolderName + "_xx.xml format");
                 break;
             }
         }
